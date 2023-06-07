da9f7cf4f0e7932a708775c86d63a2d0edea66ab
Rolando <rolurmzguerra@gmail.com>
2023-06-06 19:03:40 -0600

    Primera clase: persona

---
+++ src/POOEj01/POOEj01.java
@@ -0,0 +1,25 @@
+package POOEj01;
+
+import Entidad.Persona;
+import Servicios.PersonaService;
+
+public class POOEj01 {
+    public static void main(String[] args) {
+//        Persona primeraPersona = new Persona("Rolando", 21, "Mexicana");
+//
+//        System.out.println(primeraPersona.getNombre());
+//        primeraPersona.setNombre("Juan");
+//        System.out.println(primeraPersona.getNombre());
+//
+//        System.out.println(primeraPersona.getEdad());
+//        primeraPersona.setEdad(30);
+//        System.out.println(primeraPersona.getEdad());
+
+        PersonaService perServicio = new PersonaService();
+        Persona terceraPersona = perServicio.crearPersona();
+//        perServicio.mostrarPersona(terceraPersona);
+        System.out.println(terceraPersona.toString());
+
+
+    }
+}
---
+++ src/Servicios/PersonaService.java
@@ -0,0 +1,31 @@
+package Servicios;
+
+import Entidad.Persona;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class PersonaService {
+    Scanner read = new Scanner(in);
+
+    public Persona crearPersona() {
+        Persona personaCompleta = new Persona();
+
+        //Pedimos el nombre
+        System.out.println("Ingrese el nombre de la persona");
+        personaCompleta.setNombre(read.next());
+
+        System.out.println("Ingrese la edad de la persona");
+        personaCompleta.setEdad(read.nextInt());
+
+        System.out.println("Ingrese la nacionalidad de la persona");
+        personaCompleta.setNacionalidad(read.next());
+
+        return personaCompleta;
+    }
+    public void mostrarPersona(Persona persona) {
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getNacionalidad());
+    }
+}
