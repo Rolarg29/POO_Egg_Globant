08247f4a67d1e3332f4f2bfeb35bdd6f3ad8a465
Rolando <rolurmzguerra@gmail.com>
2023-06-28 16:49:38 -0600

    Ejercicio 12 completado

---
+++ src/Actividades/Ejercicio12/Entidad/Persona.java
@@ -0,0 +1,40 @@
+package Actividades.Ejercicio12.Entidad;
+
+import java.util.Date;
+
+public class Persona {
+    private String nombre;
+    private Date fechaNacimiento;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, Date fechaNacimiento) {
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "\n nombre='" + nombre + '\'' +
+                ", \n fechaNacimiento=" + fechaNacimiento +
+                "\n}";
+    }
+}
