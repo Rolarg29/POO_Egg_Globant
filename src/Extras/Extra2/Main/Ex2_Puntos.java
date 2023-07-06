22af2ae1d14c5e2ba964c5f313ab27b2411d0793
Rolando <rolurmzguerra@gmail.com>
2023-07-05 23:24:14 -0600

    Extra 2 completado

--- src/Extras/Extra2/Main/Ex2_Puntos.java
+++ src/Extras/Extra2/Main/Ex2_Puntos.java
@@ -1,9 +1,12 @@
 package Extras.Extra2.Main;
 
+import Extras.Extra2.Entidad.Puntos;
 import Extras.Extra2.Service.PuntosService;
 
 public class Ex2_Puntos {
     public static void main(String[] args) {
-        PuntosService ps;
+        PuntosService pService = new PuntosService();
+        Puntos puntos1 = pService.crearPuntos();
+        pService.calcularDistancia(puntos1);
     }
 }
