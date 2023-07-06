04f801f2822d8ca02e87add35838f379f7808da1
Rolando <rolurmzguerra@gmail.com>
2023-07-05 23:24:26 -0600

    Extra 3 completado

---
+++ src/Extras/Extra3/Ex3_Raices.java
@@ -0,0 +1,13 @@
+package Extras.Extra3;
+
+public class Ex3_Raices {
+    public static void main(String[] args) {
+        RaicesService rService = new RaicesService();
+        Raices raices1 = new Raices(2,5,-3);
+        Raices raices2 = new Raices(3,6,3);
+
+        rService.calcular(raices1);
+        System.out.println("==============================================");
+        rService.calcular(raices2);
+    }
+}
